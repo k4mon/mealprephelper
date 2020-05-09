54b21091d8840d6f801c4ddc360eb884f72eb6ee
Maciej Deregowski <maciej.deregowski@gmail.com>
2020-05-09 21:29:04 +0200

    extracted origin to env

--- mealprephelper/config.py
+++ mealprephelper/config.py
@@ -6,5 +6,5 @@ env = Env()
 SECRET_KEY = env("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = env.int("ACCESS_TOKEN_EXPIRE_MINUTES", 30)
-origins = ["http://localhost:3000"]
+origins = [env("ACCEPTED_ORIGIN")]
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
